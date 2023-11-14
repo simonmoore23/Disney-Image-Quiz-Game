b1d902a79926d12b5ff1c0541a0c6b0c20e501c3
Aliriza Bagdat <ambagdat@gmail.com>
2023-11-14 18:50:31 +0000

    request from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,111 @@ function showResponse(event) {
   
 // Add listener to submit element
 submitEl.addEventListener("click", showResponse);
+
+
+
+var characterMovieList = [
+  { characterName: "Dusty Crophopper", movieName: "Planes"},
+  { characterName: "Tigger", movieName: "The Many Adventures of Winnie the Pooh"},
+  { characterName: "Lilo", movieName: "Lilo & Stitch"},
+  // { characterName: "Pinocchio", movieName: "Pinocchio"},
+  // { characterName: "Dumbo", movieName: "Dumbo"},
+  // { characterName: "Peter Pan", movieName: "Peter Pan [1]"},
+  // { characterName: "Tinker Bell", movieName: "Peter Pan [1]"},
+  // { characterName: "Captain Hook", movieName: "Peter Pan [1]"},
+  // { characterName: "Mowgli", movieName: "The Jungle Book [1]"},
+  // { characterName: "Baloo", movieName: "Planes"},
+  // { characterName: "Ariel", movieName: "Planes"},
+  // { characterName: "Aladdin", movieName: "Planes"},
+  // { characterName: "Simba", movieName: "The Lion King [2]"},
+  // { characterName: "Pocahontas", movieName: "Pocahontas"},
+  // { characterName: "Flubber", movieName: "Flubber [2]"},
+  // { characterName: "Mulan", movieName: "Mulan [1]"},
+  // { characterName: "Tarzan", movieName: "Tarzan"},
+  // { characterName: "Kenai", movieName: "Brother Bear"},
+  // { characterName: "Baymax", movieName: "Big Hero 6"},
+  // { characterName: "Raya", movieName: "Raya and the Last Dragon "}
+]
+
+
+//This function removes extra information about Movie names in Brackets
+function removeBrackets(inputString) {
+  return inputString.replace(/ *\([^)]*\) */g, ''); 
+};
+
+
+//Loop to go through each object in Array
+function characterInOrder() {
+  for (let i = 0; i < characterMovieList.length; i++){
+      var currentCharacter = characterMovieList[i].characterName;
+      console.log(currentCharacter)
+      var currentMovie = characterMovieList[i].movieName;
+      
+
+      //Fetch for Character Image from API
+      var characterImage = getCharacterData(currentCharacter.characterName);
+
+      //Fetch for Movie Image from API
+      // var movieImage = getMovieData(currentCharacter.movieName);
+
+      
+
+      
+      // console.log('Movie', movieImage);
+      //Function to fetch for the character Image
+      function getCharacterData(){
+
+          var url = 'https://api.disneyapi.dev/character?name='+currentCharacter;
+          
+          fetch(url, {
+            method: 'GET',
+            
+          })
+              .then(function (response) {
+                  return response.json();
+              })
+              .then(function (data) {
+                  var characterImage = data.data.imageUrl;
+                  // var importedMovieName = data.data.films[1];
+                  // console.log(imageSource);
+                  // console.log('data:', data)
+                  console.log('CharacterImage:', characterImage);
+              });                  
+      }
+      //Function to fetch for Movie Poster
+      function getMovieData(){
+          var movieName = 'Cars';
+          var url = 'https://moviesdatabase.p.rapidapi.com/titles/search/title/'+currentMovie+'?exact=true&titleType=movie';  
+          var rapidApiHost = 'moviesdatabase.p.rapidapi.com';
+          
+          fetch(url,{
+              method:'GET',
+              headers:{
+                  'X-RapidAPI-Host': rapidApiHost,
+                  'X-RapidAPI-Key': 'e75fa08f12mshdeaa96450c2a0f2p103cc2jsn84b52f82c0ae',  
+                  'Content-Type': 'application/json',
+              },
+          })
+              .then(function (response){
+                  return response.json()
+          })
+              .then(function(data){
+                  console.log('PosterLink',data.results[0].primaryImage.url)
+          })
+      
+
+
+      }    
+
+
+
+      
+
+
+
+
+
+  }
+}
+
+characterInOrder();
